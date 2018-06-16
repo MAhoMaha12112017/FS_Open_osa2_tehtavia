d1a162860d9494f27c870d62d6d1cff0f15aa1ac
Matti Aho <ookooko@gmail.com>
2018-06-16 11:28:58 +0300

    tehtävät 2.1-2.5

--- src/index.js
+++ src/index.js
@@ -1,71 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Kurssi from './components/Kurssi';
 
 const App = () => {
-  const kurssi = {
-    nimi: 'Half Stack -sovelluskehitys',
-    osat: [
-      {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10,
-        id: 1
-      },
-      {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7,
-        id: 2
-      },
-      {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14,
-        id: 3
-      }
-    ]
-  }
+  const kurssit = [
+    {
+      nimi: 'Half Stack -sovelluskehitys',
+      id: 1,
+      osat: [
+        {
+          nimi: 'Reactin perusteet',
+          tehtavia: 10,
+          id: 1
+        },
+        {
+          nimi: 'Tiedonvälitys propseilla',
+          tehtavia: 7,
+          id: 2
+        },
+        {
+          nimi: 'Komponenttien tila',
+          tehtavia: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      nimi: 'Node.js',
+      id: 2,
+      osat: [
+        {
+          nimi: 'Routing',
+          tehtavia: 3,
+          id: 1
+        },
+        {
+          nimi: 'Middlewaret',
+          tehtavia: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Kurssi kurssi={kurssi} />
+      <h1>Opetusohjelma</h1>
+      {kurssit.map((kurssi) => <Kurssi kurssi={kurssi} key={kurssi.id}/>)}
     </div>
-  )
-}
-
-const Kurssi = ({kurssi}) => {
-  return (
-    <div>
-      <Otsikko kurssi={kurssi.nimi} />
-      <Sisalto osat={kurssi.osat} />
-      <Yhteensa osat={kurssi.osat} />
-    </div>
-  )
-}
-
-const Otsikko = (props) => {
-  return (
-    <h1>{props.kurssi}</h1>
-  );
-} 
-
-const Sisalto = ({osat}) => {
-  return (
-    <div>
-      {osat.map((osa) => <Osa osa={osa.nimi} tehtavia={osa.tehtavia} key={osa.id}/>)}
-    </div>
-  )
-} 
-
-const Osa = (props) => {
-  return (
-    <p>{props.osa} {props.tehtavia}</p>
-  )
-}
-
-const Yhteensa = ({osat}) => {
-  return (
-    <p>yhteensä<span> </span>
-      {osat.reduce((sum, osa) => sum + osa.tehtavia
-      , 0)} 
-      <span> </span>tehtävää</p>
   )
 }
 
